33c75ababaac5aaae842e20f95c0c71fc5a80a42
kennedyaquino <kennedy.eletronica.94@gmail.com>
2024-02-11 22:11:10 -0300

    refactor: refactor server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,19 +1,10 @@
 
 import express from 'express'
-
-import { Router, Request, Response } from 'express';
+import routes from './routes';
 
 const app = express();
-
-const route = Router()
-
-app.use(express.json())
-
-route.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'hello world with Typescript' })
-})
-
-app.use(route)
+app.use(express.json());
+app.use(routes);
 
 
 app.listen(3333, () => 'server running on port 3333')
